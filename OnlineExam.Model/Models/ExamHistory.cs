19fba1fbb0be7134ce58e8d96c9ab292a5caef71
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 15:23:19 +0330

    newFeature: `ExamHistory` added

---
+++ OnlineExam.Model/Models/ExamHistory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace OnlineExam.Model.Models
+{
+    public class ExamHistory : Exam
+    {
+        public DateTime PeriodStart { get; set; }
+        public DateTime PeriodEnd { get; set; }
+    }
+}
