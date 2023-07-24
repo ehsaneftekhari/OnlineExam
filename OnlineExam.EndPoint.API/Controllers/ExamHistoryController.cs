2d58fcb6ab57d809cbc8150bc129bd5522e5f03d
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 15:39:38 +0330

    newFeature: `ExamHistoryController` added

---
+++ OnlineExam.EndPoint.API/Controllers/ExamHistoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineExam.Application.Contract.IServices;
+using OnlineExam.Application.Services;
+
+namespace OnlineExam.EndPoint.API.Controllers
+{
+    [Route("api/Exam/History")]
+    [ApiController]
+    public class ExamHistoryController : ControllerBase
+    {
+        IExamHistoryService _service;
+
+        public ExamHistoryController(IExamHistoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            if (id < 0)
+                return BadRequest("id can not be less than zero");
+
+            var dto = _service.GetById(id);
+            return Ok(dto);
+        }
+    }
+}
