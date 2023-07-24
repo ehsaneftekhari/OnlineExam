065ab1405569635e6dc0ae2d28c3b7978534a9ae
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 15:31:43 +0330

    newFeature: `ShowExamHistoryDTO` added

---
+++ OnlineExam.Application.Contract/DTOs/ShowExamHistoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace OnlineExam.Application.Contract.DTOs
+{
+    public class ShowExamHistoryDTO
+    {
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool Published { get; set; }
+        public DateTime PeriodStart { get; set; }
+        public DateTime? PeriodEnd { get; set; }
+        public TimeSpan Period { get; set; }
+    }
+}
