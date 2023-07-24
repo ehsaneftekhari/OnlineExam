6487729c058ac59e61b7552c0f9d528496a1b472
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 15:25:31 +0330

    newFeature: `IExamHistoryRepository` added

---
+++ OnlineExam.Infrastructure.Contract/IRepositories/IExamHistoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface IExamHistoryRepository : IGetHistoryRepository<ExamHistory>
+    {
+
+    }
+}
