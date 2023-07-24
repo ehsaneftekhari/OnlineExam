309f5348a4288d506aff497f623df2e13ff53624
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 10:08:45 +0330

    newFeature: `MethodAttributeManager` added

---
+++ OnlineExam.Application/AttributeManagers/MethodAttributeManager.cs
@@ -0,0 +1,25 @@
+﻿namespace OnlineExam.Application.AttributeManagers
+{
+    internal class MethodAttributeManager<TAttribute> where TAttribute : Attribute
+    {
+        readonly HashSet<string> methodsNames;
+        internal MethodAttributeManager(Type classMarkerType, ReflectionHelper reflectionHelper)
+        {
+            methodsNames = new();
+
+            this.classMarkerType = classMarkerType;
+
+            reflectionHelper.GetAttributeMarkedMethodsNames(classMarkerType, typeof(TAttribute))
+                .ForEach(m => methodsNames.Add(m));
+        }
+
+        public Type classMarkerType { get; private set; }
+
+        public Type AttributeType => typeof(TAttribute);
+
+        public bool HasMethod(string name)
+        {
+            return methodsNames.TryGetValue(name, out _);
+        }
+    }
+}
