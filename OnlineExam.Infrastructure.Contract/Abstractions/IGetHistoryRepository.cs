c791131ea51209bfd12485d3c9938bb0e6f6aa53
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 15:19:54 +0330

    newFeature: `IGetHistoryRepository` added

---
+++ OnlineExam.Infrastructure.Contract/Abstractions/IGetHistoryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineExam.Infrastructure.Contract.Abstractions
+{
+    public interface IGetHistoryRepository<TEntity> where TEntity : class
+    {
+        public IQueryable<TEntity> GetAllById(int id);
+    }
+}
