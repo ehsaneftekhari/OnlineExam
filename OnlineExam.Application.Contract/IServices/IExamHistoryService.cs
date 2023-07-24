b73d1c254fcd0adda53e4c7c0fa78516cc421d72
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-24 15:32:09 +0330

    newFeature: `IExamHistoryService` added

---
+++ OnlineExam.Application.Contract/IServices/IExamHistoryService.cs
@@ -0,0 +1,10 @@
+﻿using OnlineExam.Application.Contract.DTOs;
+using OnlineExam.Application.Contract.Markers;
+
+namespace OnlineExam.Application.Contract.IServices
+{
+    public interface IExamHistoryService : IApplicationContractMarker
+    {
+        IEnumerable<ShowExamHistoryDTO> GetById(int id);
+    }
+}
