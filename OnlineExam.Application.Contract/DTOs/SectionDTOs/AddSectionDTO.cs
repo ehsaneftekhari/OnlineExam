3055f5e181107e58d0815907fc8928ac2e4f103e
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-27 08:19:48 +0330

    newFeature: `ISectionService` and `DTOs` Added

---
+++ OnlineExam.Application.Contract/DTOs/SectionDTOs/AddSectionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.SectionDTOs
+{
+    public class AddSectionDTO
+    {
+        public string Title { get; set; }
+        public int Order { get; set; }
+        public bool RandomizeQuestions { get; set; }
+        public int ExamId { get; set; }
+    }
+}
