61e5b4d0047e3b9627a8f3cb6d5484a5860bdb69
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-07-28 11:54:29 +0330

    newFeature: `PagingModel` and `ExamFilterDTO` added

---
+++ OnlineExam.Application.Contract/DTOs/ExamDTOs/ExamFilterDTO.cs
@@ -0,0 +1,17 @@
+﻿using OnlineExam.Application.Contract.DTOs.TagDTOs;
+
+namespace OnlineExam.Application.Contract.DTOs.ExamDTOs
+{
+    public class ExamFilterDTO
+    {
+        public string? Title { get; set; } = null!;
+        public DateTime? StartFrom { get; set; }
+        public DateTime? StartTo { get; set; }
+        public DateTime? EndFrom { get; set; }
+        public DateTime? EndTo { get; set; }
+        public bool? Published { get; set; }
+        public ICollection<string>? Tags { get; set; } = new List<string>();
+        public int Take { get; set; }
+        public int Skip { get; set; }
+    }
+}
---
+++ OnlineExam.Application.Contract/DTOs/ExamDTOs/PagingModel.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.ExamDTOs
+{
+    public class PagingModel<T> where T : class
+    {
+        public PagingModel(T data, long count)
+        {
+            Data = data;
+            Count = count;
+        }
+
+        public T Data { get; set; } = null!;
+        public long Count { get; set; }
+    }
+}
