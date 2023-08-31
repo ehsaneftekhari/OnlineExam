75f3ef4020f80f8ee3f2eb876d6536bc90f159e2
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-08-31 13:14:41 +0330

    newFeature: DTOs added

---
+++ OnlineExam.Application.Contract/DTOs/QuestionDTOs/AddQustionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.QuestionDTOs
+{
+    public class AddQuestionDTO
+    {
+        public int SectionId { get; set; }
+        public string Text { get; set; }
+        public string? ImageAddress { get; set; }
+        public int Score { get; set; }
+        public int DurationDays { get; set; }
+        public int DurationHours { get; set; }
+        public int DurationMinutes { get; set; }
+        public int DurationSeconds { get; set; }
+        public int Order { get; set; }
+    }
+}
