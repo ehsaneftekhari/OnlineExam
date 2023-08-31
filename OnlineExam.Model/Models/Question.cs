497d6bd90ee72e045d5883d7221cb85fc6fd080b
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-08-31 13:04:55 +0330

    newFeatur: `Question` model added

---
+++ OnlineExam.Model/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int SectionId { get; set; }
+        public Section Section { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public string? ImageAddress { get; set; }
+        public int Score { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int Order { get; set; }
+    }
+}
