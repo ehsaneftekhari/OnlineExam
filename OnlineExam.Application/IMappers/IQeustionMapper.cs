c55e0c46ca9d4d97f2301e1be3d5744a3569ac6c
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-08-31 13:17:38 +0330

    newFeature: `QuestionMapper` added

---
+++ OnlineExam.Application/IMappers/IQeustionMapper.cs
@@ -0,0 +1,12 @@
+﻿using OnlineExam.Application.Contract.DTOs.QuestionDTOs;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Application.IMappers
+{
+    public interface IQuestionMapper
+    {
+        Question? AddDTOToEntity(AddQuestionDTO? addQuestionDTO);
+        ShowQuestionDTO? EntityToShowDTO(Question? entity);
+        void UpdateEntityByDTO(Question old, UpdateQuestionDTO @new);
+    }
+}
---
+++ OnlineExam.Application/Mappers/QuestionMapper.cs
@@ -0,0 +1,77 @@
+﻿using OnlineExam.Application.Contract.DTOs.QuestionDTOs;
+using OnlineExam.Application.IMappers;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Application.Mappers
+{
+    internal class QuestionMapper : IQuestionMapper
+    {
+        private readonly ISectionMapper _sectionMapper;
+
+        public QuestionMapper(ISectionMapper sectionMapper)
+        {
+            _sectionMapper = sectionMapper;
+        }
+
+        public Question? AddDTOToEntity(AddQuestionDTO? addQuestionDTO)
+        {
+            if (addQuestionDTO != null)
+                return new()
+                {
+                    SectionId = addQuestionDTO.SectionId,
+                    Text = addQuestionDTO.Text,
+                    Score = addQuestionDTO.Score,
+                    Duration = new TimeSpan(
+                        addQuestionDTO.DurationDays
+                        ,addQuestionDTO.DurationHours
+                        ,addQuestionDTO.DurationMinutes
+                        ,addQuestionDTO.DurationSeconds),
+                    Order = addQuestionDTO.Order
+                };
+            return null;
+        }
+
+        public ShowQuestionDTO? EntityToShowDTO(Question? entity)
+        {
+            if (entity != null)
+            {
+                return new()
+                {
+                    Id = entity.Id,
+                    SectionId = entity.SectionId,
+                    Section = entity.Section,
+                    Text = entity.Text,
+                    ImageAddress = entity.ImageAddress,
+                    Score = entity.Score,
+                    Duration = entity.Duration,
+                    Order = entity.Order
+                };
+            }
+
+            return null;
+        }
+
+        public void UpdateEntityByDTO(Question old, UpdateQuestionDTO @new)
+        {
+            if (@new != null || old != null)
+            {
+                foreach(var property in typeof(UpdateQuestionDTO).GetProperties())
+                {
+                    var pName = property.Name;
+
+                    if (pName == nameof(@new.Id))
+                        continue;
+
+                    var value = property.GetValue(@new);
+
+                    if (value != null)
+                    {
+                        var entityProperty = typeof(Question)!.GetProperty(pName);
+                        entityProperty.SetValue(old, value);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
