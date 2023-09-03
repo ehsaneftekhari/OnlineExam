4c1d7b12c9b89d570cdb8b1369f19d547341d521
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-03 06:34:38 +0330

    newFeature: `TextField` model added

---
+++ OnlineExam.Model/Models/TextField.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class TextField
+    {
+        public int Id { get; set; }
+        public int TextFieldUITypeId { get; set; }
+        public TextFieldUIType TextFieldUIType { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public int? AnswerLength { get; set; }
+        public string? RegEx { get; set; }
+    }
+
+    public enum TextFieldUIType
+    {
+        TextField = 1,
+        TextArea = 2
+    }
+}
