7ec9958040b97cc057676772f2e46395fb44ec48
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-03 06:35:35 +0330

    newFeature: `TextFieldRepository` added

---
+++ OnlineExam.Infrastructure.Contract/IRepositories/ITextFieldRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface ITextFieldRepository : IAddRepository<TextField>, IGetRepository<TextField>,
+        IUpdateRepository<TextField>, IDeleteRepository<TextField>, IGetQueryableRepository<TextField>
+    { }
+}
