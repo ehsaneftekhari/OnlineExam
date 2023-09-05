fa62b5d5af49d4660cae57d54b6a124f6df96a04
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-05 23:20:00 +0330

    newFeature: `CheckFieldOption` model added

---
+++ OnlineExam.Model/Models/CheckFieldOption.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class CheckFieldOption
+    {
+        public int Id { get; set; }
+        public int Order { get; set; }
+        public int CheckFieldId { get; set; }
+        public CheckField CheckField { get; set; } = null!;
+        public int Score { get; set; }
+        public string? ImageAddress { get; set; }
+        public string? Text { get; set; }
+    }
+}
