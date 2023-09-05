bcd40ddb2580e10693a2d73fb37b8370ca330d2a
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-05 06:26:27 +0330

    newFeature: `ICheckFieldRepository` added

---
+++ OnlineExam.Infrastructure.Contract/IRepositories/ICheckFieldRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface ICheckFieldRepository : IAddRepository<CheckField>, IGetRepository<CheckField>,
+        IUpdateRepository<CheckField>, IDeleteRepository<CheckField>, IGetQueryableRepository<CheckField>
+    { }
+}
