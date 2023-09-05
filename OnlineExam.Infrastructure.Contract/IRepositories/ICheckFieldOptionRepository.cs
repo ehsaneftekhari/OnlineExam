b0429f11a7c0630596e9d341e51f5b1d56089fcf
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-05 23:33:20 +0330

    newFeature: `ICheckFieldOptionRepository` added

---
+++ OnlineExam.Infrastructure.Contract/IRepositories/ICheckFieldOptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface ICheckFieldOptionRepository : IAddRepository<CheckFieldOption>, IGetRepository<CheckFieldOption>,
+        IUpdateRepository<CheckFieldOption>, IDeleteRepository<CheckFieldOption>, IGetQueryableRepository<CheckFieldOption>
+    { }
+}
