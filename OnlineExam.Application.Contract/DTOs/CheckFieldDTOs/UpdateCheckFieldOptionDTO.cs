efb2f96c82802ac6f554008e4d42067c57380e7f
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-05 23:42:19 +0330

    newFeature: `AddCheckFieldOptionDTO` | `ShowCheckFieldOptionDTO` | `UpdateCheckFieldOptionDTO` added

---
+++ OnlineExam.Application.Contract/DTOs/CheckFieldDTOs/UpdateCheckFieldOptionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.CheckFieldDTOs
+{
+    public class UpdateCheckFieldOptionDTO
+    {
+        public int? Order { get; set; }
+        public int? CheckFieldId { get; set; }
+        public int? Score { get; set; }
+        public string? Text { get; set; }
+    }
+}
