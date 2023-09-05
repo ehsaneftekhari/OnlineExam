7b98597e88745c4765d8b0a101e0c4d99e13f6f5
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-05 06:54:30 +0330

    newFeature: `ICheckFieldMapper` added

---
+++ OnlineExam.Application/Abstractions/IMappers/ICheckFieldMapper.cs
@@ -0,0 +1,11 @@
+﻿using OnlineExam.Model.Models;
+
+namespace OnlineExam.Application.Abstractions.IMappers
+{
+    public interface ICheckFieldMapper
+    {
+        CheckField? AddDTOToEntity(int questionId, AddCheckFieldDTO? addCheckFieldDTO);
+        ShowCheckFieldDTO EntityToShowDTO(CheckField addCheckFieldDTO);
+        void UpdateEntityByDTO(CheckField old, UpdateCheckFieldDTO @new);
+    }
+}
