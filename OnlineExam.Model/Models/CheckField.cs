6bd4bf8289559b596b395e624d41bfe3fdf274ef
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-05 06:20:54 +0330

    newFeature: `CheckField` model added / `CheckFieldUIType` enum added

---
+++ OnlineExam.Model/Models/CheckField.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class CheckField
+    {
+        public int Id { get; set; }
+        public CheckFieldUIType CheckFieldUIType { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public bool RandomizeOptions { get; set; }
+        public int MaximumSelection { get; set; }
+    }
+
+    public enum CheckFieldUIType
+    {
+        TextField = 1,
+        TextArea = 2
+    }
+}
