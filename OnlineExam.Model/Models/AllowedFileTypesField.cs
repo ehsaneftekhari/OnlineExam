591428ce09b5835b533514ac7340605c4e37f438
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-06 22:47:12 +0330

    newFeature: `AllowedFileTypesField` and `FileField` added

---
+++ OnlineExam.Model/Models/AllowedFileTypesField.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class AllowedFileTypesField : BaseModel
+    {
+        public string Name { get; set; } = null!;
+        public string Extention { get; set; } = null!;
+        public ICollection<FileField> FileFields { get; set; } = new List<FileField>();
+    }
+}
---
+++ OnlineExam.Model/Models/FileField.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class FileField : BaseModel
+    {
+        public int QuestionId { get; set; }
+        public Question Question { get; set; } = null!;
+        public int KiloByteMaximumSize { get; set; }
+        public ICollection<AllowedFileTypesField> AllowedFileTypes { get; set; } = new List<AllowedFileTypesField>();
+    }
+}
