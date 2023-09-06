a445f4565d8c17ccbe78d657ab255e698bfed2ba
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-06 07:54:17 +0330

    bugFix: `CheckFieldId` removed from `UpdateCheckFieldOptionDTO`

--- OnlineExam.Application.Contract/DTOs/CheckFieldDTOs/UpdateCheckFieldOptionDTO.cs
+++ OnlineExam.Application.Contract/DTOs/CheckFieldDTOs/UpdateCheckFieldOptionDTO.cs
@@ -3,7 +3,6 @@
     public class UpdateCheckFieldOptionDTO
     {
         public int? Order { get; set; }
-        public int? CheckFieldId { get; set; }
         public int? Score { get; set; }
         public string? Text { get; set; }
     }
