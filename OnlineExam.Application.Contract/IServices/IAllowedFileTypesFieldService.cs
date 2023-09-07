0239985469d2b0cacebeff7fb7f5de1422ae1a4b
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 07:41:47 +0330

    refactor: bad naming fixed in `IAllowedFileTypesFieldService` (`GetAllId` -> `GetAll`)

--- OnlineExam.Application.Contract/IServices/IAllowedFileTypesFieldService.cs
+++ OnlineExam.Application.Contract/IServices/IAllowedFileTypesFieldService.cs
@@ -6,7 +6,7 @@ namespace OnlineExam.Application.Contract.IServices
     {
         ShowAllowedFileTypesFieldDTO Add(int questionId, AddAllowedFileTypesFieldDTO dTO);
         ShowAllowedFileTypesFieldDTO? GetById(int id);
-        IEnumerable<ShowAllowedFileTypesFieldDTO> GetAllId(int skip = 0, int take = 20);
+        IEnumerable<ShowAllowedFileTypesFieldDTO> GetAll(int skip = 0, int take = 20);
         void Update(int id, UpdateAllowedFileTypesFieldDTO dTO);
         void Delete(int id);
     }
