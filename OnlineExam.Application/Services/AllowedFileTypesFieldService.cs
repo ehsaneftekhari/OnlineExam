ca2166415dadb87faeb49e25dc0e2fff10296108
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 09:05:32 +0330

    newFeature: `AllowedFileTypesFieldService` and `FileFieldService` added

---
+++ OnlineExam.Application/Services/AllowedFileTypesFieldService.cs
@@ -0,0 +1,33 @@
+﻿using OnlineExam.Application.Contract.DTOs.AllowedFileTypesFieldDTOs;
+using OnlineExam.Application.Contract.IServices;
+
+namespace OnlineExam.Application.Services
+{
+    public class AllowedFileTypesFieldService : IAllowedFileTypesFieldService
+    {
+        public ShowAllowedFileTypesFieldDTO Add(int questionId, AddAllowedFileTypesFieldDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShowAllowedFileTypesFieldDTO> GetAll(int skip = 0, int take = 20)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShowAllowedFileTypesFieldDTO? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, UpdateAllowedFileTypesFieldDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineExam.Application/Services/FileFieldService.cs
@@ -0,0 +1,46 @@
+﻿using OnlineExam.Application.Abstractions.IMappers;
+using OnlineExam.Application.Contract.DTOs.FileFieldDTOs;
+using OnlineExam.Application.Contract.IServices;
+using OnlineExam.Infrastructure.Contract.IRepositories;
+
+namespace OnlineExam.Application.Services
+{
+    public class FileFieldService : IFileFieldService
+    {
+        readonly IFileFieldOptionRepository _optionRepository;
+        readonly IQuestionRepository _questionRepository;
+        readonly IFileFieldMapper _fileFieldMapper;
+
+        public FileFieldService(IFileFieldOptionRepository optionRepository, IQuestionRepository questionRepository, IFileFieldMapper fileFieldMapper)
+        {
+            _optionRepository = optionRepository;
+            _questionRepository = questionRepository;
+            _fileFieldMapper = fileFieldMapper;
+        }
+
+        public ShowFileFieldDTO Add(int questionId, AddFileFieldDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShowFileFieldDTO> GetAllByQuestionId(int questionId, int skip = 0, int take = 20)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShowFileFieldDTO? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, UpdateFileFieldDTO dTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
