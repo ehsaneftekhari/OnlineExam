e6e98d3e63e85829186466bb731998a41fc3e6de
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 09:02:30 +0330

    refactor: `AllowedFileTypesIds` removed from `ShowFileFieldDTO`

--- OnlineExam.Application.Contract/DTOs/FileFieldDTOs/ShowFileFieldDTO.cs
+++ OnlineExam.Application.Contract/DTOs/FileFieldDTOs/ShowFileFieldDTO.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public int QuestionId { get; set; }
         public int KiloByteMaximumSize { get; set; }
-        public ICollection<int> AllowedFileTypesIds { get; set; } = new List<int>();
         public IDictionary<int, string> AllowedFileTypesNameIdPairs { get; set; } = new Dictionary<int, string>();
     }
 }
