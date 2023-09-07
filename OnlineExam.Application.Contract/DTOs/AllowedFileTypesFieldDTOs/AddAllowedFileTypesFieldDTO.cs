bfbea8d149988902eed72e3bd8fd34b27036ab9f
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 07:34:38 +0330

    newFeature: `AddAllowedFileTypesFieldDTO` | `ShowAllowedFileTypesFieldDTO` | `UpdateAllowedFileTypesFieldDTO` added

---
+++ OnlineExam.Application.Contract/DTOs/AllowedFileTypesFieldDTOs/AddAllowedFileTypesFieldDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.AllowedFileTypesFieldDTOs
+{
+    public class AddAllowedFileTypesFieldDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Extension { get; set; } = null!;
+    }
+}
---
+++ OnlineExam.Application.Contract/DTOs/AllowedFileTypesFieldDTOs/ShowAllowedFileTypesFieldDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.AllowedFileTypesFieldDTOs
+{
+    public class ShowAllowedFileTypesFieldDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Extension { get; set; } = null!;
+    }
+}
