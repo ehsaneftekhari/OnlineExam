06139483a8eccc27f2f7e98bc0519cfe34a6265c
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 07:16:18 +0330

    newFeature: `FileFieldOptionRepository` added / `AllowedFileTypesFieldOptionRepository`

---
+++ OnlineExam.Infrastructure/Repositories/FileFieldOptionRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineExam.Infrastructure.Abstraction;
+using OnlineExam.Infrastructure.Contexts;
+using OnlineExam.Infrastructure.Contract.IRepositories;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Repositories
+{
+    public class FileFieldOptionRepository : BaseRepository<FileField>, IFileFieldOptionRepository
+    {
+        public FileFieldOptionRepository(OnlineExamContext context) : base(context) { }
+    }
+}
