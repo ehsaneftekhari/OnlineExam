65f8b83a110eb6ea32e8cc6a0e5933603d139b8f
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 07:09:02 +0330

    newFeature: `FileField` and `AllowedFileTypesField` added to `OnlineExamContext` / `Extention` typo fixed in `AllowedFileTypesField`

---
+++ OnlineExam.Infrastructure/Mappings/AllowedFileTypesEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineExam.Infrastructure.Abstraction;
+using OnlineExam.Model.Models;
+using System.Data;
+
+namespace OnlineExam.Infrastructure.Mappings
+{
+    internal class AllowedFileTypesEntityTypeConfiguration : BaseModelEntityTypeConfiguration<AllowedFileTypesField>
+    {
+        public override void ConcreteConfigure(EntityTypeBuilder<AllowedFileTypesField> builder)
+        {
+            builder.Property(x => x.Extension)
+                .IsRequired()
+                .HasColumnType(nameof(SqlDbType.NVarChar))
+                .HasMaxLength(8);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType(nameof(SqlDbType.NVarChar))
+                .HasMaxLength(512);
+        }
+    }
+}
--- OnlineExam.Model/Models/AllowedFileTypesField.cs
+++ OnlineExam.Model/Models/AllowedFileTypesField.cs
@@ -3,7 +3,7 @@
     public class AllowedFileTypesField : BaseModel
     {
         public string Name { get; set; } = null!;
-        public string Extention { get; set; } = null!;
+        public string Extension { get; set; } = null!;
         public ICollection<FileField> FileFields { get; set; } = new List<FileField>();
     }
 }
