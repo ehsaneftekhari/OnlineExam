897acdd67a59e77a7080a0390fa1888af5a9d8dd
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 07:15:28 +0330

    newFeature: `IFileFieldOptionRepository` added / `IAllowedFileTypesFieldOptionRepository` added

---
+++ OnlineExam.Infrastructure.Contract/IRepositories/IAllowedFileTypesFieldOptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface IAllowedFileTypesFieldOptionRepository : IAddRepository<AllowedFileTypesField>, IGetRepository<AllowedFileTypesField>,
+        IUpdateRepository<AllowedFileTypesField>, IDeleteRepository<AllowedFileTypesField>, IGetQueryableRepository<AllowedFileTypesField>
+    { }
+}
---
+++ OnlineExam.Infrastructure.Contract/IRepositories/IFileFieldOptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface IFileFieldOptionRepository : IAddRepository<FileField>, IGetRepository<FileField>,
+        IUpdateRepository<FileField>, IDeleteRepository<FileField>, IGetQueryableRepository<FileField>
+    { }
+}
