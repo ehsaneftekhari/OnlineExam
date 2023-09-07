b62c30841be7e43cb57e80e007deb9018d34e861
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 09:01:39 +0330

    newFeature: `IAllowedFileTypesFieldMapper` and `IFileFieldMapper`

---
+++ OnlineExam.Application/Abstractions/IMappers/IFileFieldMapper.cs
@@ -0,0 +1,12 @@
+﻿using OnlineExam.Application.Contract.DTOs.FileFieldDTOs;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Application.Abstractions.IMappers
+{
+    public interface IFileFieldMapper
+    {
+        FileField? AddDTOToEntity(int questionId, AddFileFieldDTO? addFileFieldDTO, Func<ICollection<int>, ICollection<AllowedFileTypesField>> allowedFileTypesFieldExtractor);
+        ShowFileFieldDTO EntityToShowDTO(FileField fileField);
+        void UpdateEntityByDTO(FileField old, UpdateFileFieldDTO @new, Func<ICollection<int>, ICollection<AllowedFileTypesField>> allowedFileTypesFieldExtractor);
+    }
+}
