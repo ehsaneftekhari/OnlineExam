4b51978d95cb4684aa5848ece1ebe65dbdfef8b8
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-07 07:24:14 +0330

    newFeature: `AddFileFieldDTO` | `ShowFileFieldDTO` | `UpdateFileFieldDTO` added

---
+++ OnlineExam.Application.Contract/DTOs/FileFieldDTOs/AddFileFieldDTO.cs
@@ -0,0 +1,11 @@
+﻿using OnlineExam.Model.Models;
+using OnlineExam.Model;
+
+namespace OnlineExam.Application.Contract.DTOs.FileFieldDTOs
+{
+    public class AddFileFieldDTO
+    {
+        public int KiloByteMaximumSize { get; set; }
+        public ICollection<int> AllowedFileTypesIds { get; set; } = new List<int>();
+    }
+}
