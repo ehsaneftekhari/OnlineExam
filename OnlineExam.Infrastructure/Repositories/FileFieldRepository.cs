fe9da1c434d8c0f16841156b93e46c29914e6cc7
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-08 10:24:18 +0330

    changeRequ: `GetById` and `GetIQueryable` were overrided in `FileFieldRepository`

--- OnlineExam.Infrastructure/Repositories/FileFieldRepository.cs
+++ OnlineExam.Infrastructure/Repositories/FileFieldRepository.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using OnlineExam.Infrastructure.Abstraction;
 using OnlineExam.Infrastructure.Contexts;
 using OnlineExam.Infrastructure.Contract.IRepositories;
@@ -9,5 +9,15 @@ namespace OnlineExam.Infrastructure.Repositories
     public class FileFieldRepository : BaseRepository<FileField>, IFileFieldRepository
     {
         public FileFieldRepository(OnlineExamContext context) : base(context) { }
+
+        public override FileField? GetById(int id)
+        {
+            return GetIQueryable().FirstOrDefault(x => x.Id == id);
+        }
+
+        public override IQueryable<FileField> GetIQueryable()
+        {
+            return _context.FileField.Include(x => x.AllowedFileTypes);
+        }
     }
 }
