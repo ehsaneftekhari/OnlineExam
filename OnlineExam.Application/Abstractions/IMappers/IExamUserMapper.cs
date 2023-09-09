bd2772519c62bd8badbd6250e53e754cda74be89
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-10 01:55:54 +0330

    newFeature: `IExamUserMapper` added

---
+++ OnlineExam.Application/Abstractions/IMappers/IExamUserMapper.cs
@@ -0,0 +1,11 @@
+﻿using OnlineExam.Application.Contract.DTOs.ExamUserDTOs;
+using OnlineExam.Model.Models;
+
+namespace OnlineExam.Application.Abstractions.IMappers
+{
+    public interface IExamUserMapper
+    {
+        ExamUser? AddDTOToEntity(AddExamUserDTO? dTO);
+        ShowExamUserDTO? EntityToShowDTO(ExamUser? entity);
+    }
+}
