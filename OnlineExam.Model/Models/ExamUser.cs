bcfa4b476b7bd9c147bfe297253c0f0622649aac
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-09 22:07:34 +0330

    newFeature: `ExamUser` model  Added

---
+++ OnlineExam.Model/Models/ExamUser.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineExam.Model.Models
+{
+    public class ExamUser : BaseModel
+    {
+        string UserId { get; set; } = null!;
+        int ExamId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int EarnedScore { get; set; }
+    }
+}
