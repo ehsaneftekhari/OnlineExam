0ba050e801ab8854a92c32a455f243993acaa775
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-10 01:56:28 +0330

    newFeature: `IDatabaseBasedExamUserValidator

---
+++ OnlineExam.Application/Abstractions/IValidators/IDatabaseBasedExamUserValidator.cs
@@ -0,0 +1,9 @@
+﻿using OnlineExam.Application.Contract.DTOs.ExamUserDTOs;
+
+namespace OnlineExam.Application.Abstractions.IValidators
+{
+    public interface IDatabaseBasedExamUserValidator
+    {
+        void DatabaseBasedValidate(AddExamUserDTO dTO);
+    }
+}
