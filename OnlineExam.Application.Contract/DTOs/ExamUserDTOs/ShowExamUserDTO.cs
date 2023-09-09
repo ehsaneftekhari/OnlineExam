ffd0b5965ca2f1d56713dfed2e2adba315acce31
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-09 22:26:06 +0330

    newFeature: `ShowExamUserDTO` and `AddExamUserDTO` added

---
+++ OnlineExam.Application.Contract/DTOs/ExamUserDTOs/ShowExamUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineExam.Application.Contract.DTOs.ExamUserDTOs
+{
+    public class ShowExamUserDTO
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public int ExamId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int EarnedScore { get; set; }
+    }
+}
