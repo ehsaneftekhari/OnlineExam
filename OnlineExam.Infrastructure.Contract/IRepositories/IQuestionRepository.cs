24b30c8667086babeac115f9151c0e58b725f611
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-10 23:49:06 +0330

    refactor: `IGetRepository` refactored: `TId` added

--- OnlineExam.Infrastructure.Contract/IRepositories/IQuestionRepository.cs
+++ OnlineExam.Infrastructure.Contract/IRepositories/IQuestionRepository.cs
@@ -3,7 +3,7 @@ using OnlineExam.Model.Models;
 
 namespace OnlineExam.Infrastructure.Contract.IRepositories
 {
-    public interface IQuestionRepository : IAddRepository<Question>, IGetRepository<Question>
+    public interface IQuestionRepository : IAddRepository<Question>, IGetRepository<Question, int>
         , IUpdateRepository<Question>, IDeleteRepository<Question>, IGetQueryableRepository<Question>
     {
     }
