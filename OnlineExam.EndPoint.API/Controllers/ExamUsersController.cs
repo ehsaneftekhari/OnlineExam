d766904abe7bf185fc10885658853de15b10a8f4
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-10 22:57:47 +0330

    newFeature: `ExamUsersController` added

---
+++ OnlineExam.EndPoint.API/Controllers/ExamUsersController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineExam.Application.Contract.DTOs.ExamUserDTOs;
+using OnlineExam.Application.Contract.DTOs.SectionDTOs;
+using OnlineExam.Application.Contract.IServices;
+using OnlineExam.Application.Services;
+using OnlineExam.EndPoint.API.Exceptions;
+
+namespace OnlineExam.EndPoint.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ExamUsersController : ControllerBase
+    {
+        readonly IExamUserService _examUserService;
+
+        public ExamUsersController(IExamUserService examUserService)
+        {
+            _examUserService = examUserService;
+        }
+
+        [HttpGet("Exams/{id}/[controller]")]
+        public IActionResult GetAllByExamId(int id, int pageNumber, int pageSize)
+        {
+            if (pageNumber < 1)
+                throw new APIValidationException("pageNumber can not be less than 1");
+
+            if (pageSize < 1)
+                throw new APIValidationException("pageSize can not be less than 1");
+
+            var dto = _examUserService.GetAllByExamId(id, (pageNumber - 1) * pageSize, pageSize);
+            return Ok(dto);
+        }
+
+        [HttpGet("[controller]/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var dto = _examUserService.GetById(id);
+            return Ok(dto);
+        }
+
+        [HttpPost("Exams/{id}/[controller]")]
+        public IActionResult Create(int id)
+        {
+            var examUser = new AddExamUserDTO()
+            {
+                ExamId = id,
+                UserId = "1"
+            };
+
+            return Ok(_examUserService.Add(examUser));
+        }
+
+        [HttpDelete("[controller]/{id}")]
+        public IActionResult Delete(int id)
+        {
+            _examUserService.Delete(id);
+            return Ok();
+        }
+    }
+}
