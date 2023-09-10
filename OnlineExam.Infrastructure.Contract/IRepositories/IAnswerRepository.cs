6bb899aa2e0fdcad3b87c5dc7c61f14a0c63b44e
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-10 23:50:50 +0330

    newFeature: `IAnswerRepository` added

---
+++ OnlineExam.Infrastructure.Contract/IRepositories/IAnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineExam.Infrastructure.Contract.Abstractions;
+using OnlineExam.Model.Models;
+using System.Security.Cryptography;
+
+namespace OnlineExam.Infrastructure.Contract.IRepositories
+{
+    public interface IAnswerRepository : IAddRepository<Answer>, IGetRepository<Answer, (int ExamUserId, int QuestionId)>, 
+        IUpdateRepository<Answer>, IDeleteRepository<Answer>, IGetQueryableRepository<Answer>
+    {
+
+        Answer? GetById(int ExamUserId, int QuestionId);
+    }
+}
