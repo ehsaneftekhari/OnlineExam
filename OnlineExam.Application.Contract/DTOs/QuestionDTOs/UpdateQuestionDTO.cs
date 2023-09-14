a9bef273efb537830b784aa35ea5e748c27240f8
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-14 20:55:01 +0330

    bugFix: `DurationDays` removed from `AddQuestionDTO` and `UpdateQuestionDTO`

--- OnlineExam.Application.Contract/DTOs/QuestionDTOs/UpdateQuestionDTO.cs
+++ OnlineExam.Application.Contract/DTOs/QuestionDTOs/UpdateQuestionDTO.cs
@@ -5,7 +5,6 @@
         public string? Text { get; set; }
         public string? ImageAddress { get; set; }
         public int? Score { get; set; }
-        public int? DurationDays { get; set; }
         public int? DurationHours { get; set; }
         public int? DurationMinutes { get; set; }
         public int? DurationSeconds { get; set; }
