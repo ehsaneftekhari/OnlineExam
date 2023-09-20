55d69bdfa24394acd5ba9b4a170c8de6e7051f7e
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-20 16:47:01 +0330

    changeRequ: `FileFieldService` uses `FileFieldInternalService` insteda of `IFileFieldRepository` | `AllowedFileTypesFieldService` | `IQuestionRepository`

--- OnlineExam.Application/Services/FileFieldServices/FileFieldService.cs
+++ OnlineExam.Application/Services/FileFieldServices/FileFieldService.cs
@@ -1,124 +1,48 @@
 ﻿using OnlineExam.Application.Abstractions.IMappers;
 using OnlineExam.Application.Abstractions.IValidators;
 using OnlineExam.Application.Contract.DTOs.FileFieldDTOs;
-using OnlineExam.Application.Contract.Exceptions;
 using OnlineExam.Application.Contract.IServices;
-using OnlineExam.Infrastructure.Contract.IRepositories;
 
 namespace OnlineExam.Application.Services.FileFieldServices
 {
     public class FileFieldService : IFileFieldService
     {
-        readonly IFileFieldRepository _fileFieldRepository;
-        readonly AllowedFileTypesFieldService _allowedFileTypesFieldInternalService;
-        readonly IQuestionRepository _questionRepository;
+        FileFieldInternalService _fileFieldInternalService;
         readonly IFileFieldMapper _fileFieldMapper;
         readonly IFileFieldValidator _fileFieldValidator;
 
-        public FileFieldService(IFileFieldRepository optionRepository,
-                                IQuestionRepository questionRepository,
+        public FileFieldService(FileFieldInternalService fileFieldInternalService,
                                 IFileFieldMapper fileFieldMapper,
-                                IFileFieldValidator fileFieldValidator,
-                                AllowedFileTypesFieldService allowedFileTypesFieldInternalService)
+                                IFileFieldValidator fileFieldValidator)
         {
-            _fileFieldRepository = optionRepository;
-            _questionRepository = questionRepository;
+            _fileFieldInternalService = fileFieldInternalService;
             _fileFieldMapper = fileFieldMapper;
             _fileFieldValidator = fileFieldValidator;
-            _allowedFileTypesFieldInternalService = allowedFileTypesFieldInternalService;
         }
 
         public ShowFileFieldDTO Add(int questionId, AddFileFieldDTO dTO)
         {
-            if (dTO == null)
-                throw new ArgumentNullException();
-
-            if (questionId < 1)
-                throw new ApplicationValidationException("QuestionId can not be less than 1");
-
             _fileFieldValidator.ValidateDTO(dTO);
-
-
-            var newFileField = _fileFieldMapper.AddDTOToEntity(questionId, dTO, _allowedFileTypesFieldInternalService.GetByIds)!;
-            if (_fileFieldRepository.Add(newFileField) > 0 && newFileField.Id > 0)
-                return _fileFieldMapper.EntityToShowDTO(newFileField)!;
-
-            throw new Exception();
+            var newFileField = _fileFieldMapper.AddDTOToEntity(questionId, dTO)!;
+            _fileFieldInternalService.Add(newFileField);
+            return _fileFieldMapper.EntityToShowDTO(newFileField)!;
         }
 
-        public void Delete(int id)
-        {
-            if (id < 1)
-                throw new ApplicationValidationException("id can not be less than 1");
-
-            var fileField = _fileFieldRepository.GetById(id);
-
-            if (fileField == null)
-                throw new ApplicationSourceNotFoundException($"FileField with id:{id} is not exists");
-
-            if (_fileFieldRepository.Delete(fileField) < 0)
-                throw new Exception();
-        }
+        public void Delete(int fileFieldId)
+            => _fileFieldInternalService.Delete(fileFieldId);
 
         public IEnumerable<ShowFileFieldDTO> GetAllByQuestionId(int questionId, int skip = 0, int take = 20)
+            => _fileFieldInternalService.GetAllByParentId(questionId, skip, take).Select(_fileFieldMapper.EntityToShowDTO);
+
+        public ShowFileFieldDTO? GetById(int fileFieldId)
+            => _fileFieldMapper.EntityToShowDTO(_fileFieldInternalService.GetById(fileFieldId));
+
+        public void Update(int fileFieldId, UpdateFileFieldDTO dTO)
         {
-            if (questionId < 1)
-                throw new ApplicationValidationException("examId can not be less than 1");
-
-            if (skip < 0 || take < 1)
-                throw new OEApplicationException();
-
-            var fileFields =
-                _fileFieldRepository.GetIQueryable()
-                .Where(q => q.QuestionId == questionId)
-                .Skip(skip)
-                .Take(take)
-                .ToList()
-                .Select(_fileFieldMapper.EntityToShowDTO);
-
-            if (!fileFields.Any())
-            {
-                if (_questionRepository.GetById(questionId) == null)
-                    throw new ApplicationSourceNotFoundException($"Question with id:{questionId} is not exists");
-
-                throw new ApplicationSourceNotFoundException($"there is no FileField within Question (questionId:{questionId})");
-            }
-
-            return fileFields!;
-        }
-
-        public ShowFileFieldDTO? GetById(int id)
-        {
-            if (id < 1)
-                throw new ApplicationValidationException("id can not be less than 1");
-
-            var fileField = _fileFieldRepository.GetById(id);
-
-            if (fileField == null)
-                throw new ApplicationSourceNotFoundException($"FileField with id:{id} is not exists");
-
-            return _fileFieldMapper.EntityToShowDTO(fileField);
-        }
-
-        public void Update(int id, UpdateFileFieldDTO dTO)
-        {
-            if (id < 1)
-                throw new ApplicationValidationException("id can not be less than 1");
-
-            if (dTO == null)
-                throw new ArgumentNullException();
-
             _fileFieldValidator.ValidateDTO(dTO);
-
-            var fileField = _fileFieldRepository.GetById(id);
-
-            if (fileField == null)
-                throw new ApplicationSourceNotFoundException($"FileField with id:{id} is not exists");
-
-            _fileFieldMapper.UpdateEntityByDTO(fileField, dTO, _allowedFileTypesFieldInternalService.GetByIds);
-
-            if (_fileFieldRepository.Update(fileField) <= 0)
-                throw new Exception();
+            var fileField = _fileFieldInternalService.GetById(fileFieldId);
+            _fileFieldMapper.UpdateEntityByDTO(fileField, dTO);
+            _fileFieldInternalService.Update(fileField);
         }
     }
 }
