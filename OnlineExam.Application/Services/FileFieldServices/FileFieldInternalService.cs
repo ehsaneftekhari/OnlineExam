2081e66d0261f86d1011b9e304fc5695833ca0de
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-09-20 16:01:02 +0330

    newFeature: `FileFieldInternalService` added

---
+++ OnlineExam.Application/Services/FileFieldServices/FileFieldInternalService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineExam.Application.Abstractions.InternalService;
+using OnlineExam.Application.Contract.IServices;
+using OnlineExam.Application.Services.QuestionServices;
+using OnlineExam.Infrastructure.Contract.IRepositories;
+using OnlineExam.Model.Models;
+using System.Linq.Expressions;
+
+namespace OnlineExam.Application.Services.FileFieldServices
+{
+    public class FileFieldInternalService : BaseInternalService<FileField, IFileFieldRepository, Question, IQuestionRepository>
+    {
+        public FileFieldInternalService(IFileFieldService repository, QuestionInternalService parentInternalService) : base(repository, parentInternalService){}
+
+        protected override Expression<Func<FileField, int>> ParentIdProvider => x => x.QuestionId;
+    }
+}
