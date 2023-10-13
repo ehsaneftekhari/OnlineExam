70119378bb6719b3c286e74f714e5b4fbb1c9a34
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-10-13 09:49:11 +0330

    newfeature: `ApplicationUnAuthenticateException` | `ApplicationUnAuthorizedException` added

---
+++ OnlineExam.Application.Contract/Exceptions/ApplicationUnAuthorizedException.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineExam.Application.Contract.Exceptions
+{
+    public class ApplicationUnAuthorizedException : OEApplicationException
+    {
+        public ApplicationUnAuthorizedException(string message) : base(message)
+        {
+        }
+    }
+}
