703f3fc79d80d93fe2e34052fbff13d43ece6296
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-11-10 23:21:44 +0330

    newFeature: `ValidateIfExamUserCanFinish` | `ThrowIfUserIsNotOwnerOrExamOwner` | `ThrowIfUserIsNotOwner` methods added to `IDatabaseBasedExamUserValidator` and `DatabaseBasedExamUserValidator` / `IExamInternalService` and `IExamUserInternalService` fields changed to `Lazy<IExamInternalService>` and `Lazy<IExamUserInternalService>`

--- OnlineExam.Application/Abstractions/IValidators/IDatabaseBasedExamUserValidator.cs
+++ OnlineExam.Application/Abstractions/IValidators/IDatabaseBasedExamUserValidator.cs
@@ -1,9 +1,13 @@
-using OnlineExam.Application.Contract.DTOs.ExamUserDTOs;
+﻿using OnlineExam.Application.Contract.DTOs.ExamUserDTOs;
+using OnlineExam.Model.Models;
 
 namespace OnlineExam.Application.Abstractions.IValidators
 {
     public interface IDatabaseBasedExamUserValidator
     {
         void DatabaseBasedValidateBeforeAdd(AddExamUserDTO dTO);
+        void ValidateIfExamUserCanFinish(string issuerUserId, ExamUser examUser);
+        void ThrowIfUserIsNotOwnerOrExamOwner(string issuerUserId, ExamUser examUser);
+        void ThrowIfUserIsNotOwner(int examId, string issuerUserId);
     }
 }
--- OnlineExam.Application/Validators/DatabaseBasedExamUserValidator.cs
+++ OnlineExam.Application/Validators/DatabaseBasedExamUserValidator.cs
@@ -1,36 +1,84 @@
-﻿using OnlineExam.Application.Abstractions.BaseInternalServices;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnlineExam.Application.Abstractions.BaseInternalServices;
 using OnlineExam.Application.Abstractions.IInternalService;
 using OnlineExam.Application.Abstractions.IValidators;
 using OnlineExam.Application.Contract.DTOs.ExamUserDTOs;
 using OnlineExam.Application.Contract.Exceptions;
+using OnlineExam.Application.Services.ExamServices;
+using OnlineExam.Application.Services.UserServices;
 using OnlineExam.Model.Models;
 
 namespace OnlineExam.Application.Validators
 {
     public class DatabaseBasedExamUserValidator : IDatabaseBasedExamUserValidator
     {
-        readonly IExamInternalService _examInternalService;
+        readonly Lazy<IExamInternalService> examInternalService;
+        readonly Lazy<IExamUserInternalService> examUserInternalService;
 
-        public DatabaseBasedExamUserValidator(IExamInternalService examRepository)
+        public DatabaseBasedExamUserValidator(IServiceProvider serviceProvider)
         {
-            _examInternalService = examRepository;
+            examInternalService = new (() => serviceProvider.GetRequiredService<IExamInternalService>());
+            examUserInternalService = new(() => serviceProvider.GetRequiredService<IExamUserInternalService>());
         }
 
-        public void DatabaseBasedValidate(AddExamUserDTO dTO)
-            => DatabaseBasedValidateValues(dTO.ExamId);
+        private IExamInternalService _examInternalService => examInternalService.Value;
 
-        private void DatabaseBasedValidateValues(int examId)
+        private IExamUserInternalService _examUserInternalService => examUserInternalService.Value;
+
+        public void DatabaseBasedValidateBeforeAdd(AddExamUserDTO dTO)
         {
-            var exam = _examInternalService.GetById(examId);
+            var exam = _examInternalService.GetById(dTO.ExamId);
 
             if (exam == null)
-                throw new ApplicationValidationException($"there is no Exam with id: {examId}");
+                throw new ApplicationValidationException($"there is no Exam with id: {dTO.ExamId}");
 
             if (!exam.Published)
-                throw new ApplicationValidationException($"Exam (id: {examId}) is not published");
+                throw new ApplicationValidationException($"Exam (id: {dTO.ExamId}) is not published");
 
             if (exam.End < DateTime.Now)
-                throw new ApplicationValidationException($"the Exam (id: {examId}) is finished at {exam.End}");
+                throw new ApplicationValidationException($"the Exam (id: {dTO.ExamId}) is finished at {exam.End}");
+
+            if (_examUserInternalService
+                .GetIQueryable()
+                .Where(x => x.UserId == dTO.UserId)
+                .Where(x => x.ExamId == dTO.ExamId)
+                .Any())
+                throw new ApplicationValidationException($"User (id : {dTO.UserId}) has been registered at this Exam (id: {dTO.ExamId}) before");
         }
+
+        public void ValidateIfExamUserCanFinish(string issuerUserId, ExamUser examUser)
+        {
+            if (examUser.End != null)
+                throw new ApplicationValidationException($"The Exam ({examUser.Id}) has finished for User (id : {issuerUserId})");
+
+            var exam = _examInternalService.GetById(examUser.ExamId);
+
+            var nowDateTime = DateTime.Now;
+
+            if (exam.End < nowDateTime)
+                throw new ApplicationValidationException($"The Exam ({examUser.Id}) is already finished");
+        }
+
+        public void ThrowIfUserIsNotOwnerOrExamOwner(string issuerUserId, ExamUser examUser)
+        {
+            if (examUser.UserId != issuerUserId && _examInternalService.GetById(examUser.ExamId).CreatorUserId != issuerUserId)
+                throw new ApplicationUnAuthorizedException(
+                string.Empty,
+                new ApplicationValidationException(GenerateUnAuthorizedExceptionMessage(issuerUserId)));
+        }
+
+        public void ThrowIfUserIsNotOwner(int examId, string issuerUserId)
+        {
+            if (_examInternalService.GetById(examId).CreatorUserId != issuerUserId)
+                throw new ApplicationUnAuthorizedException(
+                    string.Empty,
+                    new ApplicationValidationException(GenerateUnAuthorizedExceptionMessage(examId, issuerUserId)));
+        }
+
+        private string GenerateUnAuthorizedExceptionMessage(string issuerUserId)
+            => $"User (id : {issuerUserId}) has no access to this ExamUser";
+
+        private string GenerateUnAuthorizedExceptionMessage(int examId, string issuerUserId)
+            => $"User (id : {issuerUserId}) is not the owner of exam (id : {examId})";
     }
 }
