ccc36498dcaee1c0fc2903a8a10885a2cc408d0e
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-11-10 23:23:15 +0330

    newFeature: `Finish` method added to `IExamUserService` and `ExamUserService` / validation methods removed from `ExamUserService`

--- OnlineExam.Application.Contract/IServices/IExamUserService.cs
+++ OnlineExam.Application.Contract/IServices/IExamUserService.cs
@@ -8,5 +8,6 @@ namespace OnlineExam.Application.Contract.IServices
         ShowExamUserDTO? GetById(int id, string issuerUserId);
         IEnumerable<ShowExamUserDTO> GetAllByExamId(int examId, string issuerUserId, int skip = 0, int take = 20);
         void Delete(int id, string issuerUserId);
+        void Finish(int id, string issuerUserId);
     }
 }
