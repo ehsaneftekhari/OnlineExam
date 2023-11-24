784d7961134f34585c9e8f3155eb715fed378635
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-11-24 23:02:28 +0330

    refactor: validation function moved to `IQuestionValidator` and `QuestionValidator`

---
+++ OnlineExam.Application/Abstractions/IValidators/IQuestionValidator.cs
@@ -0,0 +1,15 @@
+﻿using OnlineExam.Model.Models;
+
+namespace OnlineExam.Application.Abstractions.IValidators
+{
+    public interface IQuestionValidator
+    {
+        public void ThrowIfUserIsNotExamCreator(int sectionId, string issuerUserId);
+
+        public void ThrowIfUserIsNotExamCreatorOrExamUser(int sectionId, string issuerUserId);
+
+        public void ThrowIfUserIsNotExamCreatorOrExamUser(string issuerUserId, Exam exam);
+
+        public void ThrowIfUserIsNotExamCreator(string issuerUserId, Exam exam);
+    }
+}
--- OnlineExam.Application/Services/QuestionServices/QuestionService.cs
+++ OnlineExam.Application/Services/QuestionServices/QuestionService.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using OnlineExam.Application.Abstractions.BaseInternalServices;
 using OnlineExam.Application.Abstractions.IInternalService;
 using OnlineExam.Application.Abstractions.IMappers;
 using OnlineExam.Application.Abstractions.IValidators;
 using OnlineExam.Application.Contract.DTOs.QuestionDTOs;
-using OnlineExam.Application.Contract.Exceptions;
 using OnlineExam.Application.Contract.IServices;
-using OnlineExam.Application.Services.SectionServices;
-using OnlineExam.Application.Validators;
 using OnlineExam.Model.Models;
 
 namespace OnlineExam.Application.Services.QuestionServices
@@ -16,61 +12,48 @@ namespace OnlineExam.Application.Services.QuestionServices
     {
         readonly IQuestionInternalService _questionInternalService;
         readonly IQuestionMapper _questionMapper;
-        readonly ISectionInternalService _sectionInternalService;
-        readonly IExamValidator _examValidator;
+        readonly IQuestionValidator _questionValidator;
 
-        public QuestionService(IQuestionInternalService questionInternalService, IQuestionMapper questionMapper, ISectionInternalService sectionInternalService, IExamValidator examValidator)
+
+        public QuestionService(IQuestionInternalService questionInternalService,
+                               IQuestionMapper questionMapper,
+                               IQuestionValidator questionValidator)
         {
             _questionInternalService = questionInternalService;
             _questionMapper = questionMapper;
-            _sectionInternalService = sectionInternalService;
-            _examValidator = examValidator;
+            _questionValidator = questionValidator;
         }
 
         public ShowQuestionDTO Add(int sectionId, string issuerUserId, AddQuestionDTO dTO)
         {
-            var exam = _sectionInternalService
-                .GetById(sectionId, _sectionInternalService
-                                        .GetIQueryable()
-                                        .Include(x => x.Exam)).Exam;
-
-            ThrowIfUserIsNotExamCreator(issuerUserId, exam);
+            _questionValidator.ThrowIfUserIsNotExamCreator(sectionId, issuerUserId);
 
             var newQuestion = _questionMapper.AddDTOToEntity(sectionId, dTO);
             _questionInternalService.Add(newQuestion!);
             return _questionMapper.EntityToShowDTO(newQuestion)!;
         }
 
-
         public IEnumerable<ShowQuestionDTO> GetAllBySectionId(int sectionId, string issuerUserId, int skip, int take)
         {
-            var exam = _sectionInternalService
-                .GetById(sectionId, _sectionInternalService
-                                        .GetIQueryable()
-                                        .Include(x => x.Exam)
-                                        .ThenInclude(x => x.ExamUsers)).Exam;
-
-            ThrowIfUserIsNotExamCreatorOrExamUser(issuerUserId, exam);
+            _questionValidator.ThrowIfUserIsNotExamCreatorOrExamUser(sectionId, issuerUserId);
 
             return _questionInternalService.GetAllByParentId(sectionId, skip, take).Select(_questionMapper.EntityToShowDTO)!;
         }
-
 
         public ShowQuestionDTO? GetById(int questionId, string issuerUserId)
         {
             var question = GetQuestionWith_Section_Exam_ExamUser_Included(questionId);
 
-            ThrowIfUserIsNotExamCreatorOrExamUser(issuerUserId, question.Section.Exam);
+            _questionValidator.ThrowIfUserIsNotExamCreatorOrExamUser(issuerUserId, question.Section.Exam);
 
             return _questionMapper.EntityToShowDTO(question);
         }
-
 
         public void Update(int questionId, string issuerUserId, UpdateQuestionDTO dTO)
         {
             var question = GetQuestionWith_Section_Exam_ExamUser_Included(questionId);
 
-            ThrowIfUserIsNotExamCreator(issuerUserId, question.Section.Exam);
+            _questionValidator.ThrowIfUserIsNotExamCreator(issuerUserId, question.Section.Exam);
 
             _questionMapper.UpdateEntityByDTO(question, dTO);
 
@@ -81,7 +64,7 @@ namespace OnlineExam.Application.Services.QuestionServices
         {
             var question = GetQuestionWith_Section_Exam_ExamUser_Included(questionId);
 
-            ThrowIfUserIsNotExamCreator(issuerUserId, question.Section.Exam);
+            _questionValidator.ThrowIfUserIsNotExamCreator(issuerUserId, question.Section.Exam);
 
             _questionInternalService.Delete(question);
         }
@@ -92,18 +75,6 @@ namespace OnlineExam.Application.Services.QuestionServices
                                         .Include(x => x.Section)
                                         .ThenInclude(x => x.Exam)
                                         .ThenInclude(x => x.ExamUsers));
-        }
-
-        private static void ThrowIfUserIsNotExamCreatorOrExamUser(string issuerUserId, Exam exam)
-        {
-            if (exam.CreatorUserId != issuerUserId
-                && !exam.ExamUsers.Any(x => x.UserId == issuerUserId))
-                throw new ApplicationUnAuthorizedException($"User has no access to Question");
-        }
-
-        private void ThrowIfUserIsNotExamCreator(string issuerUserId, Exam exam)
-        {
-            _examValidator.ThrowIfUserIsNotExamCreator(issuerUserId, exam);
         }
     }
 }
