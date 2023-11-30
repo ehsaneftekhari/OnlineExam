0774e6988df103934315a6c052109dd3878a51a9
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-11-30 16:04:09 +0330

    newFeature: `IsUserExamCreator` added to `ExamValidator` and `IExamValidator`

--- OnlineExam.Application/Abstractions/IValidators/IExamValidator.cs
+++ OnlineExam.Application/Abstractions/IValidators/IExamValidator.cs
@@ -4,6 +4,8 @@ namespace OnlineExam.Application.Abstractions.IValidators
 {
     public interface IExamValidator
     {
+        bool IsUserExamCreator(string issuerUserId, Exam exam);
+
         void ThrowIfUserIsNotExamCreator(string issuerUserId, Exam exam);
     }
 }
--- OnlineExam.Application/Validators/ExamValidator.cs
+++ OnlineExam.Application/Validators/ExamValidator.cs
@@ -6,9 +6,13 @@ namespace OnlineExam.Application.Validators
 {
     internal class ExamValidator : IExamValidator
     {
+
+        public bool IsUserExamCreator(string issuerUserId, Exam exam)
+            => exam.CreatorUserId != issuerUserId;
+
         public void ThrowIfUserIsNotExamCreator(string issuerUserId, Exam exam)
         {
-            if (exam.CreatorUserId != issuerUserId)
+            if (IsUserExamCreator(issuerUserId, exam))
                 throw new ApplicationUnAuthorizedException("User has no access to Exam");
         }
     }
